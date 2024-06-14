431865cf6a3382e4e642cd67d8f8b62985460274
Agustin <almartinez1@mail.austral.edu.ar>
2024-06-14 14:51:45 -0300

    build fix p.2

--- src/main/java/edu/austral/ingsis/math/Constant.java
+++ src/main/java/edu/austral/ingsis/math/Constant.java
@@ -6,52 +6,50 @@ import java.util.Objects;
 import java.util.Set;
 
 public final class Constant implements Function {
-    private final Double value;
+  private final Double value;
 
-    public Constant(Double value) {
-        this.value = value;
+  public Constant(Double value) {
+    this.value = value;
+  }
+
+  @Override
+  public Function solve(Map<String, Double> variablesMap) {
+    return this;
+  }
+
+  @Override
+  public Set<String> getVariables() {
+    return Collections.emptySet();
+  }
+
+  @Override
+  public String getString() {
+    if (value == value.intValue()) {
+      return String.valueOf(value.intValue());
     }
+    return value.toString();
+  }
 
-    @Override
-    public Function solve(Map<String, Double> variablesMap) {
-        return this;
-    }
+  @Override
+  public Double value() {
+    return value;
+  }
 
-    @Override
-    public Set<String> getVariables() {
-        return Collections.emptySet();
-    }
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    var that = (Constant) obj;
+    return Objects.equals(this.value, that.value);
+  }
 
-    @Override
-    public String getString() {
-        if (value == value.intValue()) {
-            return String.valueOf(value.intValue());
-        }
-        return value.toString();
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
 
-    @Override
-    public Double value() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Constant) obj;
-        return Objects.equals(this.value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return "Constant[" +
-                "value=" + value + ']';
-    }
-
+  @Override
+  public String toString() {
+    return "Constant[" + "value=" + value + ']';
+  }
 }
