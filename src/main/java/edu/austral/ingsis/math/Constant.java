02bb3143af3457cb05d033a43cd7f0e440c1b5ab
Agustin <almartinez1@mail.austral.edu.ar>
2024-06-14 14:53:08 -0300

    build fix p.3

--- src/main/java/edu/austral/ingsis/math/Constant.java
+++ src/main/java/edu/austral/ingsis/math/Constant.java
@@ -37,8 +37,12 @@ public final class Constant implements Function {
 
   @Override
   public boolean equals(Object obj) {
-    if (obj == this) return true;
-    if (obj == null || obj.getClass() != this.getClass()) return false;
+    if (obj == this) {
+      return true;
+    }
+    if (obj == null || obj.getClass() != this.getClass()) {
+      return false;
+    }
     var that = (Constant) obj;
     return Objects.equals(this.value, that.value);
   }
